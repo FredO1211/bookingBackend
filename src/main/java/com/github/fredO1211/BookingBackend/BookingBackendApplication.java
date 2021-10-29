21bf6c008af24247ad8c106eff3a0b18f54402b3
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-10-29 21:14:41 +0200

    initial commit

---
+++ src/main/java/com/github/fredO1211/BookingBackend/BookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.fredO1211.BookingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fredO1211/BookingBackend/BookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fredO1211.BookingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
